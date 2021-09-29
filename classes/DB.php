abcd66b02ea84bb7f6a1557fbb445ce4004525fd
dominik.halvonik <dominik.halvonik@gmail.com>
2021-09-29 10:33:53 +0200

    Hodina 1

--- classes/DB.php
+++ classes/DB.php
@@ -4,29 +4,5 @@ namespace Classes;
 
 class DB
 {
-    private $host;
-    private $username;
-    private $pass;
-    private $dbName;
-    private $port;
-
-    private $connection;
-
-    public function __construct($host, $username, $pass, $dbName, $port)
-    {
-        $this->host = $host;
-        $this->username = $username;
-        $this->pass = $pass;
-        $this->dbName = $dbName;
-        $this->port = $port;
-
-        try {
-            $this->connection = new \PDO("mysql:host=".$this->host.";port=".$this->port."dbname=".$this->dbName, $this->username, $this->pass);
-            // set the PDO error mode to exception
-            $this->connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-        } catch(\PDOException $e) {
-            echo "Connection failed: " . $e->getMessage();
-        }
-    }
 
 }
